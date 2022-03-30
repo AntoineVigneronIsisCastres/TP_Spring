12adf9a501f62a8bc768386a5ff01ca74acef3a4
AntoineVigneronIsisCastres <antoine.vigneron@etud.univ-jfc.fr>
2022-03-30 13:58:48 +0200

    Added DAO queries and test implementations

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -1,6 +1,12 @@
 package monprojet.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import monprojet.entity.City;
 import monprojet.entity.Country;
@@ -9,5 +15,25 @@ import monprojet.entity.Country;
 //
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
+    
+    @Query(value = "SELECT SUM(City.POPULATION) "
+        + "FROM Country, City "
+        + "WHERE Country.ID = :id "
+        +" AND Country.ID = City.COUNTRY_ID",
+        nativeQuery = true)
+    public List<Integer> populationDuPays (String id);
 
+    public interface PaysParPopulation {
+        String getName();
+        int getPopulation();
+    }
+
+    @Query(value = "SELECT Country.NAME, SUM(City.POPULATION) "
+        + "FROM Country, City "
+        + "WHERE Country.ID = City.COUNTRY_ID "
+        + "GROUP BY Country.NAME",
+        nativeQuery = true)
+    public List<PaysParPopulation> populationDeChaquePays();
+    
 }
+
